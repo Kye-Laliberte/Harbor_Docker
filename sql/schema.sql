f90b2bf094f5a40fc934ffbf0b84c404fb700669
Kye Laliberte <klaliber@icloud.com>
2026-07-22 17:57:59 -0500

    refactor: update schema to correct table references and constraints for ships, docks, and dockings

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,50 +4,55 @@
 --    experience_years INTEGER CHECK(experience_years >= 0) DEFAULT 0
 --);
 
+
+
 CREATE TABLE if NOT EXISTS ships(
     id SERIAL PRIMARY KEY,
 --    captain_id INTEGER  REFERENCES Captain(id),
-    ship_status status DEFAULT 'docked' CHECK (ship_status IN ('docked', 'sailing', 'maintenance')),
+    ship_status STATUS  CHECK (ship_status IN ('docked', 'sailing', 'maintenance')),
     ship_name Text DEFAULT 'Unknown Ship',
     current_cargo INTEGER NOT NULL CHECK (current_cargo >= 0),
     registration_number TEXT UNIQUE NOT NULL,
-    cargo_capacity INTEGER NOT NULL CHECK (cargo_capacity >= 0)
+    cargo_capacity INTEGER NOT NULL CHECK (cargo_capacity >= 0),
+    CHECK(curent_cargo <= cargo_capacity)
 );
 
-CREATE TABLE if NOT EXISTS dock(
-    id SERIAL PRIMARY KEY,
-    dock_code INTEGER UNIQUE NOT NULL,
-    dock_status status DEFAULT 'active' CHECK (dock_status IN ('active', 'inactive', 'maintenance')),
-    harbor_id INTEGER REFERENCES Harbor(id),
-    port_name TEXT NOT NULL,
-    cargo_capacity INTEGER NOT NULL CHECK (cargo_capacity >= 0)
-);
 
 CREATE TABLE if NOT EXISTS harbors(
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    harbor_status status DEFAULT 'inactive' CHECK (harbor_status IN ('active', 'inactive'))
+    timezone TIMESTAMP WITH TIME ZONE NOT NULL 
+   -- harbor_status dock_harbor DEFAULT 'inactive' CHECK (harbor_status IN ('active', 'inactive', 'maintenance'))
 );
 
 CREATE TABLE if NOT EXISTS voyage(
    id SERIAL PRIMARY KEY,
-   ship_id INTEGER NOT NULL REFERENCES Ship(id),
-   departure_harbor_id INTEGER NOT NULL REFERENCES Harbor(id),
-   destination_harbor_id INTEGER NOT NULL REFERENCES Harbor(id) CHECK( destination_harbor_id <> departure_harbor_id),
-   departure_time TIMESTAMP,
-   estimated_arrival TIMESTAMP,
-   actual_arrival TIMESTAMP,
-   travel_status TEXT NOT NULL CHECK (travel_status IN ('scheduled','departed','arrived','cancelled'))
+   ship_id INTEGER NOT NULL REFERENCES ships(id),
+   departure_harbor_id INTEGER NOT NULL REFERENCES harbors(id),
+   destination_harbor_id INTEGER REFERENCES harbors(id),
+   departure_time TIMESTAMP WITH TIME ZONE,
+   estimated_arrival TIMESTAMP WITH TIME ZONE,
+   actual_arrival TIMESTAMP WITH TIME ZONE,
+   travel_status STATUS NOT NULL CHECK (travel_status IN ('scheduled','departed','arrived','cancelled'))
+    --CHECK( destination_harbor_id <> departure_harbor_id)
+);
+
+CREATE TABLE if NOT EXISTS docks(
+    id SERIAL PRIMARY KEY,
+    dock_code INTEGER UNIQUE NOT NULL,
+    dock_status STATUS NOT NULL DEFAULT 'active' CHECK (dock_status IN ('active', 'inactive', 'maintenance')),
+    harbor_id INTEGER REFERENCES Harbor(id),
+    dock_name TEXT NOT NULL,
+    cargo_capacity INTEGER NOT NULL CHECK (cargo_capacity >= 0)
 );
 
 CREATE TABLE if NOT EXISTS dockings(
     id SERIAL PRIMARY KEY,
-    ship_id INTEGER REFERENCES Ship(id),
-    dock_id INTEGER REFERENCES Dock(id),
+    ship_id INTEGER REFERENCES ships(id),
+    dock_id INTEGER REFERENCES docks(id),
     arrival_date TIMESTAMP NOT NULL,
     departure_date TIMESTAMP,
     purpose TEXT,
-    ship_clearance_status status DEFAULT 'pending' CHECK (ship_clearance_status IN ('pending', 'approved', 'denied'))
-    CHECK(
-        departure_date IS NULL OR departure_date >= arrival_date)
+    ship_clearance_status status DEFAULT 'pending' CHECK (ship_clearance_status IN ('pending', 'approved', 'denied')),
+    CHECK(departure_date IS NULL OR departure_date >= arrival_date)
 );
