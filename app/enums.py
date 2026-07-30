f31f40da368879cc177e3308b054214d88403356
Kye Laliberte <klaliber@icloud.com>
2026-07-30 18:50:47 -0500

    added: enums for the difrent  status cheks in the sql A models

---
+++ app/enums.py
@@ -0,0 +1,32 @@
+﻿from enum import Enum
+
+
+class DockStatus(Enum):
+    ACTIVE = 'active'
+    INACTIVE = 'inactive'
+    MAINTENANCE = 'maintenance'
+
+
+class VesselSize(Enum):
+    SMALL = 'small'
+    MEDIUM = 'medium'
+    LARGE = 'large'
+
+
+class ShipStatus(Enum):
+    DOCKED = 'docked'
+    SAILING = 'sailing'
+    MAINTENANCE = 'maintenance'
+
+
+class ShipClearanceStatus(Enum):
+    PENDING = 'pending'
+    APPROVED = 'approved'
+    DENIED = 'denied'
+
+
+class VoyageStatus(Enum):
+    SCHEDULED = 'scheduled'
+    DEPARTED = 'departed'
+    ARRIVED = 'arrived'
+    CANCELLED = 'cancelled'
