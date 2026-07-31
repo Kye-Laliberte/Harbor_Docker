e3100cca56fd1759da818cb3dc24b4b67cc97090
Kye Laliberte <klaliber@icloud.com>
2026-07-30 19:56:16 -0500

    set up routes in main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.routes.docks import router as docks_router
+from app.routes.dockings import router as dockings_router
+from app.routes.harbors import router as harbors_router
+from app.routes.ships import router as ships_router
+from app.routes.voyages import router as voyages_router
+
+app = FastAPI(title="Harbor API")
+
+app.include_router(ships_router)
+app.include_router(harbors_router)
+app.include_router(docks_router)
+app.include_router(dockings_router)
+app.include_router(voyages_router)
 
 
 @app.get("/")
